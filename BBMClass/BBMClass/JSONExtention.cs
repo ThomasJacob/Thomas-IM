95b971747fcb83e63e57fbf2a159faa7eabbcb8a
ThomasJacob-IdentityMine <thomas.jacob@identitymine.com>
2014-03-13 16:45:05 +0530

    JSONExtention added

---
+++ BBMClass/BBMClass/JSONExtention.cs
@@ -0,0 +1,43 @@
+﻿using Newtonsoft.Json.Linq;
+
+namespace com.bbm.Helpers
+{
+    public static class JSONExtention
+    {
+        public static string optString(this JObject jObject, string key, string defaultValue)
+        {
+            JToken token = jObject[key];
+            return token == null ? defaultValue : (string)token;
+        }
+
+        public static bool optBoolean(this JObject jObject, string key, bool defaultValue)
+        {
+            JToken token = jObject[key];
+            return token == null ? defaultValue : (bool)token;
+        }
+
+        public static double optDouble(this JObject jObject, string key, double defaultValue)
+        {
+            JToken token = jObject[key];
+            return token == null ? defaultValue : (double)token;
+        }
+
+        public static int optInt(this JObject jObject, string key, int defaultValue)
+        {
+            JToken token = jObject[key];
+            return token == null ? defaultValue : (int)token;
+        }
+
+        public static long optInt(this JObject jObject, string key, long defaultValue)
+        {
+            JToken token = jObject[key];
+            return token == null ? defaultValue : (long)token;
+        }
+
+        public static bool has(this JObject jObject, string key)
+        {
+            return jObject[key] == null ? false : true;
+        }
+    }
+}
+
